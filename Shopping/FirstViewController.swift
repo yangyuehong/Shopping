cc989b8fef43c141651ed066bbce63b1b0ac706b
Yang, Yuehong | OSPD <ts-yuehong.yang@rakuten.com>
2017-07-05 14:12:19 +0900

    Initial Commit

---
+++ Shopping/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Shopping
+//
+//  Created by Yang, Yuehong | OSPD on 2017/07/05.
+//  Copyright © 2017年 Yang, Yuehong | OSPD. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
